190b47203a95bc09be34aaae5d6f031bec085317
vaishkash <137786788+omAtDV@users.noreply.github.com>
2025-01-04 02:11:54 +0530

    adding credentials to auth config page

--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,7 @@
-import GitHub from "next-auth/providers/github";
+import Credentials  from "next-auth/providers/credentials";
 import type { NextAuthConfig } from "next-auth";
 
 // Notice this is only an object, not a full Auth.js instance
 export default {
-  providers: [GitHub],
+  providers: [],
 } satisfies NextAuthConfig;
