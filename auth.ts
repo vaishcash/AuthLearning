6bd57081551d87db56c8ed7708fbd97883be7633
vaishkash <137786788+omAtDV@users.noreply.github.com>
2025-01-13 02:17:40 +0530

    update auth, add admin/servr/client/setting pages

--- auth.ts
+++ auth.ts
@@ -1,15 +1,19 @@
 import NextAuth from "next-auth";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { getUserById } from "./data/user";
-import { db } from "./lib/db";
-import authConfig from "./auth.config";
 import { UserRole } from "@prisma/client";
-import { getTwoFactorConfirmationByUserId } from "./data/two-factor-confirmation";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+import { db } from "@/lib/db";
+import authConfig from "@/auth.config";
+import { getUserById } from "@/data/user";
+import { getTwoFactorConfirmationByUserId } from "@/data/two-factor-confirmation";
+import { getAccountByUserId } from "./data/account";
+
 export const {
   handlers: { GET, POST },
   auth,
   signIn,
   signOut,
+  update,
 } = NextAuth({
   pages: {
     signIn: "/auth/login",
@@ -18,56 +22,53 @@ export const {
   events: {
     async linkAccount({ user }) {
       await db.user.update({
-        where: {
-          id: user.id,
-        },
-        data: {
-          emailVerified: new Date(),
-        },
+        where: { id: user.id },
+        data: { emailVerified: new Date() },
       });
     },
   },
-
   callbacks: {
-
-    async signIn({ user  , account }) {
-      console.log({ user, account });
-
-      if ( account?.provider !== "credentials") return true ;
+    async signIn({ user, account }) {
+      // Allow OAuth without email verification
+      if (account?.provider !== "credentials") return true;
 
       const existingUser = await getUserById(user.id);
 
-
-// prevent signin without email verification
-      
+      // Prevent sign in without email verification
       if (!existingUser?.emailVerified) return false;
 
-    if (existingUser.isTwoFactorEnabled) {
-        const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(existingUser.id);
+      if (existingUser.isTwoFactorEnabled) {
+        const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(
+          existingUser.id
+        );
 
         if (!twoFactorConfirmation) return false;
 
-
-// delete two factor confirmation for new user
-
+        // Delete two factor confirmation for next sign in
         await db.twoFactorConfirmation.delete({
-            where: {
-                id: twoFactorConfirmation.id,
-            },
-        })
-    }
+          where: { id: twoFactorConfirmation.id },
+        });
+      }
 
       return true;
     },
-
     async session({ token, session }) {
-      console.log({ sessionToken: token });
       if (token.sub && session.user) {
         session.user.id = token.sub;
       }
 
       if (token.role && session.user) {
         session.user.role = token.role as UserRole;
+      }
+
+      if (session.user) {
+        session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
+      }
+
+      if (session.user) {
+        session.user.name = token.name;
+        session.user.email = token.email as string;
+        session.user.isOAuth = token.isOAuth as boolean;
       }
 
       return session;
@@ -78,14 +79,19 @@ export const {
       const existingUser = await getUserById(token.sub);
 
       if (!existingUser) return token;
+
+      const existingAccount = await getAccountByUserId(existingUser.id);
+
+      token.isOAuth = !!existingAccount;
+      token.name = existingUser.name;
+      token.email = existingUser.email;
       token.role = existingUser.role;
+      token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
 
       return token;
     },
   },
   adapter: PrismaAdapter(db),
-  session: {
-    strategy: "jwt",
-  },
+  session: { strategy: "jwt" },
   ...authConfig,
 });
--- next-auth.d.ts
+++ next-auth.d.ts
@@ -2,11 +2,16 @@
 import { UserRole } from "@prisma/client";
 import  { DefaultSession } from "next-auth";
 
-export type ExistingUser = DefaultSession["user"] & { role: UserRole };
+export type ExtendedUser = DefaultSession["user"] & { role: UserRole 
 
+  isTwoFactorEnabled : boolean;
+
+  isOAuth : boolean;
+
+};
 
 declare module "next-auth" {
   interface Session {
-    user : ExistingUser;
+    user: ExtendedUser;
   }
 }
