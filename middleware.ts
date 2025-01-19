afd769b09c948da21f69c0d19861400baabc984c
vaishkash <137786788+omAtDV@users.noreply.github.com>
2025-01-19 20:28:05 +0530

    fix bug

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth";
+import NextAuth , { Session } from "next-auth";
 
 import authConfig from "./auth.config";
 import {
@@ -7,44 +7,44 @@ import {
   authRoutes,
   publicRoutes,
 } from "./routes";
+import { NextRequest } from "next/server";
+
 
 const { auth } = NextAuth(authConfig);
 
-export default auth((req) => {
+export default auth(
+  (req: NextRequest & { auth: Session | null }): Response | void => {
+    const { nextUrl } = req;
 
+    const isLoggedIn = !!req.auth;
+    // Ensure `req.auth` is properly set up in NextAuth
+    const isApiAuthRoute = nextUrl.pathname.startsWith(
+      apiAuthPrefix as unknown as string
+    );
+    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-
-
-  const { nextUrl } = req;
-
-  const isLoggedIn = !!req.auth;
-  // Ensure `req.auth` is properly set up in NextAuth
-  const isApiAuthRoute = nextUrl.pathname.startsWith(
-    apiAuthPrefix as unknown as string
-  );
-  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
-  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
-
-  // If it's an API Auth route, allow access
-  if (isApiAuthRoute) {
-    return null;
-  }
-
-  // Handle authentication routes
-  if (isAuthRoute) {
-    if (isLoggedIn) {
-      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    // If it's an API Auth route, allow access
+    if (isApiAuthRoute) {
+      return ;
     }
-    return null;
-  }
 
-  // Redirect unauthenticated users away from private routes
-  if (!isLoggedIn && !isPublicRoute) {
-    return Response.redirect(new URL("/auth/login", nextUrl));
-  }
+    // Handle authentication routes
+    if (isAuthRoute) {
+      if (isLoggedIn) {
+        return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+      }
+      return ;
+    }
 
-  return null;
-});
+    // Redirect unauthenticated users away from private routes
+    if (!isLoggedIn && !isPublicRoute) {
+      return Response.redirect(new URL("/auth/login", nextUrl));
+    }
+
+    return ;
+  }
+);
 
 export const config = {
   matcher: ["/((?!.*\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
